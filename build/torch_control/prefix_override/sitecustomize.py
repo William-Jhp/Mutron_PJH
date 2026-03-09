372c0498c34ee550295c966cdddee33628ebf3da
william-JHP <jhpark@teamgrit.kr>
2026-03-09 09:33:24 +0900

    Add gitignore

--- build/torch_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/pjh/Teamgrit2026/Mutron/install/torch_control'
--- build/torch_control/share/torch_control/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/pjh/Teamgrit2026/Mutron/build/torch_control"
